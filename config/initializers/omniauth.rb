afd1638cca15bce3188372349397d9a23fee9a17
Yuda Cogati <yudapc@gmail.com>
2016-06-07 13:16:42 +0700

    fixed issue csrf detected when authorize from dashboard api

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 require 'moka'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :mokaprovider, ENV['MOKA_APP_ID'], ENV['MOKA_SECRET_KEY']
+  provider :mokaprovider, ENV['MOKA_APP_ID'], ENV['MOKA_SECRET_KEY'], { :provider_ignores_state => true }
 end
