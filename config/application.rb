c96daee10a2948f5319bd5a9b491bbc9e7f179cf
Muhammad Yafi <yafithekid@users.noreply.github.com>
2018-08-03 17:09:07 +0700

    add api list (#18)
    re-add gemfile
    
    remove sqlite3
    
    remove db

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# require 'rails/all'
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+require "rails/test_unit/railtie"
+require "sprockets/railtie"
 require 'dotenv'
 
 Bundler.require(*Rails.groups)
@@ -8,6 +14,6 @@ Dotenv.load
 
 module ApiClient
   class Application < Rails::Application
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
